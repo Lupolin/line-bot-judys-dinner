39f4bd5a15836dce3e9b4d79bf67f8275faa6255
Lupolin <100400096+Lupolin@users.noreply.github.com>
2025-06-11 10:32:30 +0800

    Update app.py: change port from 5000 to 5002

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def reply(event, text):
 if __name__ == "__main__":
     from scheduler import scheduler
     init_db()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5002)
